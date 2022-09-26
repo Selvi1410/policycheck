cbef53283c179ffd741ccd144a79ddcc94f524d0
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-09-26 11:56:35 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "4.32.0"
     }
   }
 }
 
 provider "aws" {
+  # Configuration options
   region = var.aws_region
-  
 }
 
 resource "aws_instance""myec2" {
